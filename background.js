024839ce8d0bed08114b4f2354ab81742fe6f3a8
wtimme <wtimme@users.noreply.github.com>
2020-12-18 08:37:00 +0100

    Initial commit

---
+++ background.js
@@ -0,0 +1,26 @@
+function logURL(requestDetails) {
+  if (requestDetails.url.includes('safelinks.protection.outlook.com/')) {
+    var originalURL = getParameterByName('url', requestDetails.url);
+
+    return {
+      redirectUrl: originalURL
+    };
+  }
+}
+
+browser.webRequest.onBeforeRequest.addListener(
+  logURL,
+  {urls: ["<all_urls>"]},
+  ['blocking']
+);
+
+// Source: https://stackoverflow.com/a/901144
+function getParameterByName(name, url) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
