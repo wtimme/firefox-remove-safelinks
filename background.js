8d18e73879e01bc4c3697798e1eae396d6666c63
wtimme <wtimme@users.noreply.github.com>
2022-12-29 15:43:04 +0100

    Automatic code formatting

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 function removeSafelink(requestDetails) {
   if (requestDetails.url.includes('safelinks.protection.outlook.com/') ||
-      requestDetails.url.includes('statics.teams.cdn.office.net/evergreen-assets/safelinks/1/atp-safelinks.html')) {
+    requestDetails.url.includes('statics.teams.cdn.office.net/evergreen-assets/safelinks/1/atp-safelinks.html')) {
     var originalURL = getParameterByName('url', requestDetails.url);
 
     return {
@@ -11,17 +11,17 @@ function removeSafelink(requestDetails) {
 
 browser.webRequest.onBeforeRequest.addListener(
   removeSafelink,
-  {urls: ["<all_urls>"]},
+  { urls: ["<all_urls>"] },
   ['blocking']
 );
 
 // Source: https://stackoverflow.com/a/901144
 function getParameterByName(name, url) {
-    name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
 
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
