b1d14a46f15a4f9b5f7f46b1e782d618ad8f92fb
wtimme <wtimme@users.noreply.github.com>
2023-05-01 17:43:04 +0200

    Add support for "https://*.safelinks.protection.office365.us/*" (#8)

--- background.js
+++ background.js
@@ -16,6 +16,7 @@ browser.webRequest.onBeforeRequest.addListener(
       "https://safelinks.protection.outlook.com/*",
       "https://*.safelinks.protection.outlook.com/*",
       "https://outlook.office.com/mail/safelink.html*",
+      "https://*.safelinks.protection.office365.us/*",
     ]
   },
   ['blocking']
