58a2c050bd84bc85d90c25d54df4ab7fb39d11d7
Lukasz Czajczyk <lukasz.czajczyk@gmail.com>
2024-10-16 19:32:04 +0200

    Added options to only bypass allowed sites
    By default current behavior is maintained

---
+++ options/options.js
@@ -0,0 +1,25 @@
+const allowedHostsTextArea = document.querySelector("#allowed-hosts");
+
+// Store the currently selected settings using browser.storage.local.
+function storeSettings() {
+  let allowedHosts = allowedHostsTextArea.value.split("\n");
+  browser.storage.local.set({
+    allowedHosts
+  });
+}
+
+// Update the options UI with the settings values retrieved from storage,
+// or the default settings if the stored settings are empty.
+function updateUI(restoredSettings) {
+  allowedHostsTextArea.value = restoredSettings.allowedHosts.join("\n");
+}
+
+function onError(e) {
+  console.error(e);
+}
+
+// On opening the options page, fetch stored settings and update the UI with them.
+browser.storage.local.get().then(updateUI, onError);
+
+// Whenever the contents of the textarea changes, save the new values
+allowedHostsTextArea.addEventListener("change", storeSettings);
